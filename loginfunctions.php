32a6c2a2fef67aed0f3b9f387fb5dc2e8a4f5bc7
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-05-30 02:27:17 +0800

    Update loginfunctions.php

--- loginfunctions.php
+++ loginfunctions.php
@@ -207,7 +207,7 @@ if (isset($_POST['reset-password'])) {
     mail($to, $subject, $msg, $headers);
     header('location: pending.php?email=' . $email);*/
 	  
-    require_once('PHPMailerAutoload.php');
+    include_once('PHPMailerAutoload.php');
  
     $mail = new PHPMailer();
  
