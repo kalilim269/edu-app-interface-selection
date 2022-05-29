dd4fd14e0629dea1fe4b7a4cda38097a0f87ddfe
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-05-30 02:19:31 +0800

    Update loginfunctions.php

--- loginfunctions.php
+++ loginfunctions.php
@@ -234,7 +234,7 @@ if (isset($_POST['reset-password'])) {
     $mail->Body    = 'Click On This Link to Reset Password '.$link.'';
     if($mail->Send())
     {
-      echo "Check Your Email and Click on the link sent to your email";
+      //echo "Check Your Email and Click on the link sent to your email";
       header('location: pending.php?email=' . $email);
     }
     else
