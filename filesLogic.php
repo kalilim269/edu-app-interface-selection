e1701e64690b490f2dd6c51aef6b348ef3e69132
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-06-25 07:36:20 +0800

    Update filesLogic.php

--- filesLogic.php
+++ filesLogic.php
@@ -1,4 +1,4 @@
-<link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
