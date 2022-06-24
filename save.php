f44072656214f0b06920748b208856ecaf8a830f
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-06-25 07:49:38 +0800

    Update save.php

--- save.php
+++ save.php
@@ -26,6 +26,9 @@ include_once 'filesLogic.php';
     <![endif]-->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" rel="stylesheet" />
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
+   <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 <style type="text/css">
 @import url(https://fonts.googleapis.com/css?family=Open+Sans);
