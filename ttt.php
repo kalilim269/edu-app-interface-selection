d4541d282bdd6bb49b8e103f2c33df0b62f02e1a
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-06-26 02:26:18 +0800

    Add files via upload

---
+++ ttt.php
@@ -0,0 +1,28 @@
+<?php
+$dir = 'www';
+$ftp_server = 'lrgs.ftsm.ukm.my'; // change this
+    $ftp_user_name = 'a176496'; // change this
+    $ftp_user_pass = 'bigwhiterabbit'; // change this
+    $remote_dir = 'ftp://lrgs.ftsm.ukm.my/file_uploads/'; // change this
+    //$src_file = $_FILES['srcfile']['name'];
+
+// set up basic connection
+$ftp = ftp_connect($ftp_server);
+
+// login with username and password
+$login_result = ftp_login($ftp, $ftp_user_name, $ftp_user_pass);
+
+// try to create the directory $dir
+if (ftp_mkdir($ftp, $dir)) {
+    ftp_put($ftp, $dir, 'aa.pdf', FTP_BINARY);
+ echo "successfully created $dir\n";
+} else {
+ echo "There was a problem while creating $dir\n";
+}
+
+// close the connection
+ftp_close($ftp);
+
+?>
+
+
