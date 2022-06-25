619ef595c63d8c7c8b2b9f2346b07514b9720917
kalilim269 <98217122+kalilim269@users.noreply.github.com>
2022-06-25 08:58:22 +0800

    Update filesLogic.php

--- filesLogic.php
+++ filesLogic.php
@@ -42,7 +42,7 @@ $files = mysqli_fetch_all($result, MYSQLI_ASSOC);
 if (isset($_POST['save'])) { // if save button on the form is clicked
     // name of the uploaded file
     $filename = $_FILES['myfile']['name'];
-    chmod('file_uploads/', 0777);
+  
 
     // destination of the file on the server
     $destination = dirname(__FILE__) . "/file_uploads/" . $filename;
@@ -68,6 +68,7 @@ if (isset($_POST['save'])) { // if save button on the form is clicked
     } else {
         // move the uploaded (temporary) file to the specified destination
         if (move_uploaded_file($file, $destination)) {
+           chmod($destination, 0755);
             $sql = "INSERT INTO tbl_eduapp_files_data(file_name, file_size, downloads_count, user_id) VALUES ('$filename', $size, 0, $user)";
             if (mysqli_query($conn, $sql)) {
                 
